57c6d319a596e44d29ab1352c1b9502213a4f37d
Nazarov Dmitry <Difat1@inbox.ru>
2023-06-09 00:32:29 +0300

    update dist

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const port = process.env.PORT || 3000;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
@@ -9,7 +10,7 @@ async function bootstrap() {
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true,
     });
-    await app.listen(3000);
+    await app.listen(port, "0.0.0.0");
 }
 bootstrap();
 //# sourceMappingURL=main.js.map

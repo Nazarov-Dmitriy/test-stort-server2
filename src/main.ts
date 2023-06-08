ee6a2cd84e5321d30167f8997ff366d9eb8c241b
Nazarov Dmitry <Difat1@inbox.ru>
2023-06-09 00:27:46 +0300

    update host port

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
